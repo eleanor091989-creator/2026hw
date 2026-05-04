5cd9aa0e0bd50ed54dfd6f212d396635e318a739
eleanor091989-creator <eleanor091989@gmail.com>
2026-05-04 19:20:32 +0800

    Add Firebase Storage to configuration

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,20 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.12.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.12.0/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/12.12.0/firebase-storage.js";
+
 const firebaseConfig = {
-apiKey: "AIzaSyCgt4XVsPxcI2XfKKdCEWxZTHgFOX2tRew",
-authDomain: "hr-backoffice-shared.firebaseapp.com",
-projectId: "hr-backoffice-shared",
-storageBucket: "hr-backoffice-shared.firebasestorage.app",
-messagingSenderId: "816300633926",
-appId: "1:816300633926:web:5841f03a3936c563fbff47"
+  apiKey: "你的 apiKey",
+  authDomain: "你的 authDomain",
+  projectId: "你的 projectId",
+  storageBucket: "你的 storageBucket",
+  messagingSenderId: "你的 messagingSenderId",
+  appId: "你的 appId"
 };
+
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+export { app, auth, db, storage };
